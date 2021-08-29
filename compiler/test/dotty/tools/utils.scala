9f9fea8367280384dcc6fc7f182f33c38317da2b
Dale Wijnand <dale.wijnand@gmail.com>
2021-08-29 09:04:50 +0100

    Read tool args in test files

--- compiler/test/dotty/tools/utils.scala
+++ compiler/test/dotty/tools/utils.scala
@@ -3,8 +3,9 @@ package tools
 
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.{Files, Path => JPath}
 
-import scala.io.Source
+import scala.io.{Codec, Source}
 import scala.reflect.ClassTag
 import scala.util.Using.resource
 import scala.util.chaining.given
@@ -43,3 +44,26 @@ def assertThrows[T <: Throwable: ClassTag](p: T => Boolean)(body: => Any): Unit 
     case failed: T       => throw AssertionError(s"Exception failed check: $failed").tap(_.addSuppressed(failed))
     case NonFatal(other) => throw AssertionError(s"Wrong exception: expected ${implicitly[ClassTag[T]]} but was ${other.getClass.getName}").tap(_.addSuppressed(other))
 end assertThrows
+
+// inspect given files for tool args of the form `tool: args`
+// if args string ends in close comment, drop the `*` `/`
+// if split, parse the args string as command line.
+// (from scala.tools.partest.nest.Runner#toolArgsFor)
+def toolArgsFor(files: List[JPath])(implicit codec: Codec = Codec.UTF8): List[String] =
+  import scala.jdk.OptionConverters._
+  import dotc.config.CommandLineParser.tokenize
+  files.flatMap { path =>
+    val tag  = "scalac:"
+    val endc = "*" + "/"    // be forgiving of /* scalac: ... */
+    def stripped(s: String) = s.substring(s.indexOf(tag) + tag.length).stripSuffix(endc)
+    val args = scala.util.Using.resource(Files.lines(path, codec.charSet))(
+      _.limit(10).filter { s =>
+           s.contains("// " + tag)
+        || s.contains("/* " + tag)
+        || s.contains(" * " + tag)
+        // but avoid picking up comments like "% scalac ./a.scala" and "$ scalac a.scala"
+      }.map(stripped).findAny.toScala
+    )
+    args.map(tokenize).getOrElse(Nil)
+  }
+end toolArgsFor
