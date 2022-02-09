d3f52de21927896c27657a5957dba41b9c07fbb4
Seth Tisue <seth@tisue.net>
2022-02-09 09:06:22 -0800

    comment

--- tests/pos/13490.scala
+++ tests/pos/13490.scala
@@ -6,3 +6,23 @@ object Test:
 object Test2:
   import Test.MyEnum.A
   def foo: Any = A
+
+
+/*
+
+before:
+  Test:
+    final def MyEnum: MyApi.MyEnum.type = MyApi.MyEnum
+    @Child[(MyApi.MyEnum.A : MyApi.MyEnum)] final type MyEnum = MyApi.MyEnum
+  Test2:
+    def foo: Any = MyApi.MyEnum#A
+
+[[syntax trees at end of                   erasure]] // tests/pos/13490.scala
+
+after:
+  Test:
+    final def MyEnum(): MyApi.MyEnum = MyApi.MyEnum   // singleton type widened
+  Test2:
+    def foo(): Object = ((): MyApi.MyEnum)#A
+
+ */
